540dd6d445f35bf5cf6ab88251dd5da14e2b0913
Jason Morrison <jmorrison@thoughtbot.com>
2009-04-23 20:30:18 -0400

    putting the vars in once place

--- get.rb
+++ get.rb
@@ -1,11 +1,13 @@
 TOP_LEFT_LAT = 42.37
 TOP_LEFT_LNG = -71.12
+SPAN = 0.02
+SIZE = 640
 KEY = File.read('key')
 
-def get(i, j, span, size)
-  lat = TOP_LEFT_LAT + ( span * i )
-  lng = TOP_LEFT_LNG + ( span * j )
-  url = "http://maps.google.com/staticmap?center=#{lat},#{lng}&span=#{span},#{span}&size=#{size}x#{size}&key=#{KEY}&sensor=false"
+def get(i, j)
+  lat = TOP_LEFT_LAT + ( SPAN * i )
+  lng = TOP_LEFT_LNG + ( SPAN * j )
+  url = "http://maps.google.com/staticmap?center=#{lat},#{lng}&span=#{SPAN},#{SPAN}&size=#{SIZE}x#{SIZE}&key=#{KEY}&sensor=false"
   arg = "\"#{url}\""
   cmd = "wget #{arg} -O #{i}-#{j}.jpg"
 
